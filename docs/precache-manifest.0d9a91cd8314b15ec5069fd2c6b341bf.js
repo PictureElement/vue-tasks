31a4abdbeb1ce86acffe89d4d12add800372d842
Marios Sofokleous <marios.sofokleous@yandex.com>
2018-07-02 23:56:16 +0300

    Add docs directory for GitHub Pages

---
+++ docs/precache-manifest.0d9a91cd8314b15ec5069fd2c6b341bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68e465f3bdfae45536662bc04a096c51",
+    "url": "index.html"
+  },
+  {
+    "revision": "a45bba6d5dd8e77e56a4",
+    "url": "chunk-vendors.a45bba6d.js"
+  },
+  {
+    "revision": "a45bba6d5dd8e77e56a4",
+    "url": "chunk-vendors.5e7942cf.css"
+  },
+  {
+    "revision": "7eeaeeef2a97178e422b",
+    "url": "app.7eeaeeef.js"
+  },
+  {
+    "revision": "7eeaeeef2a97178e422b",
+    "url": "app.04b5211f.css"
+  }
+];
